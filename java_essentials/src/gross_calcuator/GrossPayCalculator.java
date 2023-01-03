c98ccb4f6e6799e6b0f50332fcf3e7043753852d
Yul310 <hsy3102@gmail.com>
2023-01-03 12:47:05 -0500

    update

--- java_essentials/src/gross_calcuator/GrossPayCalculator.java
+++ java_essentials/src/gross_calcuator/GrossPayCalculator.java
@@ -8,6 +8,10 @@ public class GrossPayCalculator {
         int hours = 40;
         //2.Get the hourly pay rate
         double payRate = 25.50;
+        //3.Multiply hours and pay rate
+        double grossPay= hours*payRate;
+        //4.Display result
+        System.out.println("Gross pay:"+ grossPay);
 
     }
 
