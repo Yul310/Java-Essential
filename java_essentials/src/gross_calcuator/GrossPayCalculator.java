23fc00b98f5da898fb3524012bbcc48aefee4e83
Yul310 <hsy3102@gmail.com>
2023-01-03 11:25:04 -0500

    initiation

---
+++ java_essentials/src/gross_calcuator/GrossPayCalculator.java
@@ -0,0 +1,15 @@
+package gross_calcuator;
+
+public class GrossPayCalculator {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        //1.Get the number of hours worked
+        int hours = 40;
+        //2.Get the hourly pay rate
+        double payRate = 25.50;
+
+    }
+
+
+}
