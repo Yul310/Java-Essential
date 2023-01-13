09111d2d3024cee7908d76ebed1b60360a43b8fa
Yul310 <hsy3102@gmail.com>
2023-01-13 11:23:18 -0500

    game updated

--- java_essentials/src/rollTheDie_game/rollTheDieGame.java
+++ java_essentials/src/rollTheDie_game/rollTheDieGame.java
@@ -7,11 +7,13 @@ public class rollTheDieGame {
 
     public static void main(String[] arg){
 
+        int leftnum = 20;
+        int leftroll = 5;
         int total = 0;
         Scanner scanner = new Scanner(System.in);
 
         for(int i = 1; i<6; i++){
-            System.out.println("Do you want to roll the dice? This is your turn " + i + ". Press Y key to proceed.");
+            System.out.println("Do you want to roll the dice? This is going to be your turn " + i + ". Press Y key to proceed.");
 
             String answer = scanner.next();
 
@@ -22,18 +24,22 @@ public class rollTheDieGame {
 
                 System.out.println("die is "+ die );
                 total += die;
+                leftnum -= die;
+                leftroll -= 1;
+
                 if(total > 20){
                     System.out.println(" 20 exceeded. you lose! " );
                     break;
                 }
             }
-
-            System.out.println("you still have "+(5-i)+" turns and you are "+(20-total)+" away." );
+            if(leftroll != 0) {
+                System.out.println(String.format(" %d turns left: you are %d away.", leftroll, leftnum));
+            }
         }
         scanner.close();
 
-        if(total < 20){
-            System.out.println(" 20 exceeded. you lose! " );
+        if(total > 20 || total < 20){
+            System.out.println(" you lose! " );
 
         }
         else if(total == 20){
